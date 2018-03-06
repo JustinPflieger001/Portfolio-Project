a6df7a8931c099c01bbc9d6bfe2089df0d70cd54
Pflieger Justin <150094741@cart.ads>
2018-03-06 15:19:36 -0800

    idk, stuff i guess

--- js.js
+++ js.js
@@ -6,3 +6,8 @@ function d_close() {
     document.getElementById("dropdown").style.display = "none";
     document.getElementById("dropup").style.display = "block";
 }
+
+function animation() {
+    document.getElementById("dropdown").classList.remove('w3-animate-top');
+    document.getElementById("dropdown").classList.add('w3-animate-bottom');
+}
