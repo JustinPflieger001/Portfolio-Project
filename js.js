db08812f92ab8dfbcb84a9e3948987601264ec49
Justin Pflieger <Justin.Pflieger@student.cart.org>
2018-03-08 21:14:47 -0800

    started new nav

--- js.js
+++ js.js
@@ -1,13 +1,8 @@
-function d_open() {
-    document.getElementById("dropdown").style.display = "block";
-    document.getElementById("dropup").style.display = "none";
-}
-function d_close() {
-    document.getElementById("dropdown").style.display = "none";
-    document.getElementById("dropup").style.display = "block";
+function openNav() {
+    document.getElementById("mySidenav").style.height.innerHTML = "25%";
 }
 
-function animation() {
-    document.getElementById("dropdown").classList.remove('w3-animate-top');
-    document.getElementById("dropdown").classList.add('w3-animate-bottom');
+function closeNav() {
+    document.getElementById("mySidenav").style.height.innerHTML = "0";
+
 }
