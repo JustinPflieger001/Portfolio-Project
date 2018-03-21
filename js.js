d3cf37f699c16b850b02e5fb66ae3bc23bb8103f
Pflieger Justin <150094741@cart.ads>
2018-03-21 15:24:31 -0700

    slideshow

--- js.js
+++ js.js
@@ -7,27 +7,35 @@ function closeNav() {
     document.getElementById("mySidenav").style.marginTop = "-180px";
 }
 
+
+    var count = 0;
 function slidee() {
-    var a = document.getElementById("slone").style.marginRight;
-    var b = document.getElementById("sltwo").style.marginRight;
-    var c = document.getElementById("slthree").style.marginRight;
 
-    if (a == "0px") {
-      document.getElementById("slone").style.marginLeft = "3000px";
-      b = "0px";
-    }
+    var s = document.getElementById("slides").children;
+    switch (count) {
+        case 0:
+            count = 1;
+            break;
+        case 1:
+            s[0].classList.add("no");
+            s[1].classList.remove("no");
+            count = 2;
+            break;
+        case 2:
+            s[1].classList.add("no");
+            s[2].classList.remove("no");
+            count = 3;
+            break;
+        case 3:
+            s[2].classList.add("no");
+            s[0].classList.remove("no");
+            count = 1;
+            break;
+        default:
+            count = 0;
+            break;
 
-    else if (b == "0px") {
-      document.getElementById("sltwo").style.marginLeft = "3000px";
-      c = "0px";
-      document.getElementById("slone").style.marginLeft = "0px";
-      a = "-3000px";
     }
-
-    else if (c == "0px") {
-      document.getElementById("slthree").style.marginLeft = "3000px";
-      a = "0px";
-      document.getElementById("sltwo").style.marginLeft = "0px";
-      b = "0px";
-    }
+    console.log(s);
+     setTimeout(slidee, 3000);
 }
